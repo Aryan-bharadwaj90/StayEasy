dbce87351f2265d9160db71b86b4ad76002948b7
Aryan-Bharadwaj90 <aryanbharadwaj691@gmail.com>
2025-07-13 16:17:39 +0530

    changes

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ const io = new Server(server, {
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 
 
 app.use("/api/auth", require("./Routes/authRoutes"));
